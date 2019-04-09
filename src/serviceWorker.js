833c81bc8848f1d165983e596ca7c72f40b0f87e
Ayush Gupta <35876280+Ayushtwentysix@users.noreply.github.com>
2019-04-09 20:19:00 +0530

    Update serviceWorker.js

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -140,22 +140,7 @@ window.addEventListener('beforeinstallprompt', (e) => {
 
 });
 
-// btnAdd.addEventListener('click', (e) => {
-//   // hide our user interface that shows our A2HS button
-//   btnAdd.style.display = 'none';
-//   // Show the prompt
-//   deferredPrompt.prompt();
-//   // Wait for the user to respond to the prompt
-//   deferredPrompt.userChoice
-//     .then((choiceResult) => {
-//       if (choiceResult.outcome === 'accepted') {
-//         console.log('User accepted the A2HS prompt');
-//       } else {
-//         console.log('User dismissed the A2HS prompt');
-//       }
-//       deferredPrompt = null;
-//     });
-// });
+
 
 
 
